e2f5823ab8528f7dd0cc0854b3379d691e021a62
makar <makar.peter98@gmail.com>
2026-09-14 16:21:02 +0200

    pressMeApp létrehozva

---
+++ src/main/java/org/example/szfm_masodik_ora/pressMeApp.java
@@ -0,0 +1,16 @@
+package org.example.szfm_masodik_ora;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class pressMeApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
